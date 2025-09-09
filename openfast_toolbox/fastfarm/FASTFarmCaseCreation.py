4f88018c45628028f3cfed7460efb4fabd69001d
Yuksel-Rudy <yuksel.alkarem@maine.edu>
2025-09-09 16:06:19 -0600

    bug fix

--- openfast_toolbox/fastfarm/FASTFarmCaseCreation.py
+++ openfast_toolbox/fastfarm/FASTFarmCaseCreation.py
@@ -615,7 +615,7 @@ class FFCaseCreation:
                 hydroF_  = self.allCond['hydroForce'].isel(cond=cond).values
             
                 # Set current path name string
-                condStr = f'Cond{cond:02d}_v{Vhub_:04.1f}_PL{shear_}_TI{tivalue_}_Hs{waveHs_:04.1f}_Tp{waveTp_:04.1f}_HF{hydroF_:+05.1f}'
+                condStr = f'Cond{cond:02d}_v{Vhub_:04.1f}_PL{shear_}_TI{tivalue_}_Hs{waveHs_:04.1f}_Tp{waveTp_:04.1f}_HF{hydroF_:06.1f}'
             else:
                 condStr = f'Cond{cond:02d}_v{Vhub_:04.1f}_PL{shear_}_TI{tivalue_}'                
             condDirList.append(condStr)
